1a0f5c89a2c1fc744598c86771bd4b727b764b10
Thai Pangsakulyanont @ Taskworld <thai.p@taskworld.com>
2018-08-01 17:17:44 +0700

    :tada::tada:

--- README.md
+++ README.md
@@ -22,5 +22,21 @@ wget https://hyper-install.s3.amazonaws.com/hyper-linux-x86_64.tar.gz
 tar xzf hyper-linux-x86_64.tar.gz
 sudo mkdir -p /data/bin
 sudo mv hyper /data/bin
+
+# Checkout repo
+sudo mkdir -p /data/src
+sudo chown circleci:circleci /data/src
+git clone https://github.com/taskworld/hyper-selenium-cleaner.git
+
+# Create config
+sudo mkdir -p /data/config/hyper
+sudo chown circleci:circleci /data/config/hyper
+cat > /data/config/hyper/config.json <<'END'
+  [paste contents of ~/.hyper/config.json here]
+END
+
+# Check if it works!
+alias hyper='/data/bin/hyper --config=/data/config/hyper'
+hyper ps
 ```
 
---
+++ clean.js
@@ -0,0 +1,46 @@
+// @ts-check
+const { execFileSync, spawnSync } = require('child_process')
+
+// const HYPER = 'hyper'
+// const HYPER_OPTS = ['-R', 'eu-central-1']
+
+const HYPER = '/data/bin/hyper'
+const HYPER_OPTS = ['--config=/data/config/hyper']
+
+const agentsRaw = execFileSync(
+  HYPER,
+  [
+    ...HYPER_OPTS,
+    'ps',
+    '-f',
+    'label=com.taskworld.hyper-selenium.role=agent',
+    '--format={{ .ID }} {{ .CreatedAt }}',
+  ],
+  {
+    encoding: 'utf8',
+    timeout: 10000,
+  }
+)
+const agents = []
+agentsRaw.replace(
+  /([0-9a-f]+) (\d\d\d\d-\d\d-\d\d) (\d\d:\d\d:\d\d) (\+\d\d\d\d)/g,
+  (__, containerId, date, time, tz) => {
+    const created = new Date(date + 'T' + time + tz)
+    agents.push({ containerId, created })
+    return ''
+  }
+)
+
+for (const agent of agents) {
+  const durationSeconds = ((Date.now() - agent.created) / 1000).toFixed(1)
+  console.log(`* ${agent.containerId} ran for ${durationSeconds}s`)
+}
+
+for (const agent of agents.filter(a => Date.now() - a.created >= 600e3)) {
+  console.log(`* Killing ${agent.containerId} — it ran for too long`)
+  // spawnSync(HYPER, [...HYPER_OPTS, 'rm', '-f', agent.containerId], {
+  //   stdio: 'inherit',
+  //   shell: true,
+  //   timeout: 10000,
+  // })
+}
